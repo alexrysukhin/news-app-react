4dc58013904137a39fe92c24b36ee4c14aab31b0
Aleksandr <kruziatus@gmail.com>
2022-10-06 20:39:32 +0300

    minor settings

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { ManagerNews } from "./pages/ManagerNews"
+import { Layout } from "./components/Layout"
+
+function App() {
+  return (
+    <>
+      <Layout />
+      <ManagerNews />
+    </>
+  );
+}
+
+export default App;
---
+++ src/type.tsx
@@ -0,0 +1,56 @@
+import { type } from "@testing-library/user-event/dist/type"
+
+export type Post = {
+    apiUrl: string
+    fields: {
+        body: string
+        bodyText: string
+        byline: string
+        bylineHtml: string
+        charCount: string
+        firstPublicationDate: string
+        headline: string
+        isInappropriateForSponsorship: boolean
+        isLive: boolean
+        isPremoderated: boolean
+        lang: string
+        lastModified: string
+        legallySensitive: boolean
+        main: string
+        productionOffice: string
+        publication: string
+        shortUrl: string
+        shouldHideAdverts: boolean
+        shouldHideReaderRevenue: boolean
+        showAffiliateLinks: boolean
+        showInRelatedContent: boolean
+        standfirst: string
+        thumbnail: string
+        trailText: string
+        wordcount: string
+
+    }
+    id: string
+    isHosted: boolean
+    pillarId: string
+    pillarName: string
+    sectionId: string
+    sectionName: string
+    tags: Tag[]
+    type: string
+    webPublicationDate: string
+    webTitle: string
+    webUrl: string
+
+}
+
+type Tag = {
+    apiUrl: string
+    id: string
+    references: string[]
+    sectionId: string
+    sectionName: string
+    type: string
+    webTitle: string
+    webUrl: string
+}
